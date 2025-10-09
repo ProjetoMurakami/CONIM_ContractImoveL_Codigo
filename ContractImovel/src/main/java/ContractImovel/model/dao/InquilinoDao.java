302ad6029f9b5c73d0f2fef4ebaebc521d93d9f9
RebecaAlves013 <161368776+RebecaAlves013@users.noreply.github.com>
2025-10-08 21:12:36 -0300

    Inquilino dao

---
+++ ContractImovel/src/main/java/ContractImovel/model/dao/InquilinoDao.java
@@ -0,0 +1,64 @@
+package ContractImovel.model.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ContractImovel.model.Imovel;
+import ContractImovel.model.Inquilino;
+import ContractImovel.model.Pagamento;
+import ContractImovel.util.jpa.Transactional;
+
+public class InquilinoDao implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Inject
+    private EntityManager manager;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InquilinoDao.class);
+
+    @Transactional
+    public Inquilino salvar(Inquilino inquilino) throws PersistenceException{
+
+        LOGGER.info("salvar DAO... pagamento = " + inquilino);
+
+        try {
+			return manager.merge(inquilino);
+		} catch (PersistenceException e) {
+			e.printStackTrace();
+			throw e;
+		}
+    }
+
+    @Transactional
+    public void excluir(Inquilino inquilino) throws PersistenceException{
+
+        try {
+			Inquilino i = manager.find(Inquilino.class, inquilino.getId());
+			manager.remove(i);
+			manager.flush();
+		} catch (PersistenceException e) {
+			e.printStackTrace();
+			throw e;
+		} 
+    }
+
+    public Inquilino buscarPeloCodigo(Long id) {
+		return manager.find(Inquilino.class, id);
+	}
+
+     @SuppressWarnings("unchecked")
+     public List<Inquilino> buscarTodos() {
+		String query="select i from Inquilino i";
+		
+		Query q = manager.createQuery(query);
+		
+		return q.getResultList();
+	}	
+}
