265d0979ca85a7558d31209cdde506e38eaab945
RebecaAlves013 <161368776+RebecaAlves013@users.noreply.github.com>
2025-10-08 21:26:11 -0300

    ManterInquilinoService

---
+++ ContractImovel/src/main/java/ContractImovel/service/ManterInquilinoService.java
@@ -0,0 +1,30 @@
+package ContractImovel.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import ContractImovel.model.Imovel;
+import ContractImovel.model.Inquilino;
+import ContractImovel.model.dao.InquilinoDao;
+
+public class ManterInquilinoService implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+	@Inject	
+	private InquilinoDao inquilinoDao;
+
+    public void salvar(Inquilino inquilino){
+        inquilinoDao.salvar(inquilino);
+    }
+
+    public void excluir(Inquilino inquilino){
+        this.inquilinoDao.excluir(inquilino);
+    }
+
+    public List<Inquilino> buscarTodos() {
+		
+		return inquilinoDao.buscarTodos();
+	}
+}
